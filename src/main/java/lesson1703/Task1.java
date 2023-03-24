6da21cc13a9b2414e8db70e882ffdd8ecb0910aa
Your Name <you@example.com>
2023-03-24 23:20:23 +0800

    added Task1 Task2

--- src/main/java/lesson1703/Task1.java
+++ src/main/java/lesson1703/Task1.java
@@ -1,4 +1,51 @@
 package lesson1703;
 
+import org.example.Main;
+
+import java.util.*;
+
 public class Task1 {
+//    Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
+
+    public static void main(String[] args) {
+        Map<String, List<String>> phoneDirectory = new HashMap<>();
+        inputNewEntry(phoneDirectory);
+        printPhoneDirectory(phoneDirectory);
+
+    }
+
+    private static void printPhoneDirectory(Map<String, List<String>> phoneDirectory) {
+        for (Map.Entry<String, List<String>> listEntry : phoneDirectory.entrySet()) {
+            System.out.printf("%s - %s\n", listEntry.getKey(), listEntry.getValue());
+        }
+
+    }
+
+    private static Map inputNewEntry(Map<String, List<String>> phoneDirectory) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Если хотите сделать запись в телефонную книгу, напишите 'еще'.\n " +
+                    "Если хотите выйти, напишите 'выход'. ");
+            String text = scanner.nextLine();
+
+            try {
+                if (text.equalsIgnoreCase("еще")) {
+                    System.out.print("Введите фамилию: ");
+                    String entryKey = scanner.nextLine();
+                    System.out.print("Введите номер телефона, если несколько номеров, то через ', ' : ");
+                    List<String> entryValue = Arrays.stream(scanner.nextLine().split(",")).toList();
+                    phoneDirectory.put(entryKey, entryValue);
+                } else if (text.equalsIgnoreCase("выход")) {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Если хотите сделать запись в телефонную книгу, напишите 'еще'.\n " +
+                        "Если хотите выйти, напишите 'выход'. ");
+            }
+        }
+        scanner.close();
+        return phoneDirectory;
+    }
+
 }
--- src/main/java/lesson1703/Task2.java
+++ src/main/java/lesson1703/Task2.java
@@ -1,4 +1,62 @@
 package lesson1703;
 
+import com.sun.jdi.Value;
+import org.example.Main;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Task2 {
+//    Пусть дан список сотрудников:
+
+//    Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений. Отсортировать по убыванию популярности.
+
+    public static void main(String[] args) {
+        List<String> workers = List.of("Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина",
+                "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова",
+                "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова",
+                "Иван Мечников", "Петр Петин", "Иван Ежов");
+
+        Map<String, Integer> nameMap = directoryNameWorker(workers);
+        sortedDirectoryNameWorker(nameMap);
+
+    }
+
+    private static void sortedDirectoryNameWorker(Map<String, Integer> nameMap) {
+        LinkedList<String> collect = new LinkedList<>(nameMap.entrySet().stream().
+                sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).
+                map(entry -> entry.getKey()).collect(Collectors.toList()));
+        for (int i = 0; i < collect.size(); i++) {
+            String currentName = collect.get(i);
+            System.out.printf("%s - %d\n", currentName, nameMap.get(currentName));
+        }
+    }
+
+    private static Map directoryNameWorker(List<String> workers) {
+        List<String> namesWorkers = new ArrayList<>();
+        for (String worker : workers) {
+            int ind = worker.indexOf(" ");
+            String nameWorker = worker.substring(0, ind);
+            namesWorkers.add(nameWorker);
+        }
+        Collections.sort(namesWorkers);
+
+        Map<String, Integer> nameMap = new TreeMap<>();
+
+        int count = 1;
+        String nameWorker = namesWorkers.get(0);
+        for (int i = 1; i < namesWorkers.size(); i++) {
+            String currentName = namesWorkers.get(i);
+            if (currentName.equals(nameWorker)) {
+                count++;
+            } else {
+                nameMap.put(nameWorker, count);
+                nameWorker = currentName;
+                count = 1;
+            }
+        }
+        nameMap.put(nameWorker, count);
+        return nameMap;
+    }
+
 }
